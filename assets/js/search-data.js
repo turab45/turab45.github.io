bea75accad80000052fc6d8e52e6b0079f0e578e
turab45 <50545053+turab45@users.noreply.github.com>
2026-02-25 00:39:13 +0000

    Deploying to gh-pages from @ turab45/turab45.github.io@c26ff65973ef65a3f46d8c5429a36af1964b4e67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -571,32 +571,11 @@ ninja.data = [{
           window.open("mailto:%74%75%72%61%62%62%61%6A%65%65%72%32%30%32 [%61%74] %67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=LD8VtagAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://turab45.github.io", "_blank");
         },
       },{
       id: 'light-theme',
