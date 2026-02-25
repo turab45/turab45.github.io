06039b829cf43cfe6e7608dc26b1c72a96a3e79b
turab45 <50545053+turab45@users.noreply.github.com>
2026-02-25 00:18:20 +0000

    Deploying to gh-pages from @ turab45/turab45.github.io@078c350aa11fb86aecc802a2b7cae644ac255459 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -561,14 +561,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/turab_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%75%72%61%62%62%61%6A%65%65%72%32%30%32 [%61%74] %67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -589,14 +589,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=LD8VtagAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.turab45.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
